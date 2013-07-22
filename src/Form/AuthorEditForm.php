2e7b9c122d832e04bb616199222f170837dccf3c
Lin Zongshu <lin40553024@163.com>
2013-07-22 14:50:47 +0800

    Added author controller

---
+++ src/Form/AuthorEditForm.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Article module AuthorEditForm form
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Copyright (c) http://www.eefocus.com
+ * @license         http://www.xoopsengine.org/license New BSD License
+ * @author          Lijun Dong <lijun@eefocus.com>
+ * @author          Zongshu Lin <zongshu@eefocus.com>
+ * @since           1.0
+ * @package         Module\Article
+ */
+
+namespace Module\Article\Form;
+
+use Pi;
+use Pi\Form\Form as BaseForm;
+
+/**
+ * Class for initializing form of add author page
+ */ 
+class AuthorEditForm extends BaseForm
+{
+    /**
+     * Initalizing form 
+     */
+    public function init()
+    {
+        $this->add(array(
+            'name'       => 'name',
+            'options'    => array(
+                'label' => __('Name'),
+            ),
+            'attributes' => array(
+                'id'   => 'name',
+                'type' => 'text',
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'photo',
+            'options' => array(
+                'label' => __('Photo'),
+            ),
+            'attributes' => array(
+               
+            ),
+        ));
+
+        $this->add(array(
+            'name' => 'description',
+            'options' => array(
+                'label' => 'Biography',
+            ),
+            'attributes' => array(
+                'id'   => 'bio',
+                'type' => 'textarea',
+            ),
+        ));
+
+        $this->add(array(
+            'name'  => 'x',
+            'attributes' => array(
+                'id'   => 'x',
+                'type' => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name'  => 'y',
+            'attributes' => array(
+                'id'   => 'y',
+                'type' => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name'  => 'w',
+            'attributes' => array(
+                'id'   => 'w',
+                'type' => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name'  => 'h',
+            'attributes' => array(
+                'id'   => 'h',
+                'type' => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name'  => 'security',
+            'type'  => 'csrf',
+        ));
+
+        $this->add(array(
+            'name'  => 'id',
+            'attributes'    => array(
+                'id'   => 'id',
+                'type' => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name'  => 'fake_id',
+            'attributes'    => array(
+                'id'   => 'fake_id',
+                'type' => 'hidden',
+            ),
+        ));
+
+        $this->add(array(
+            'name'          => 'submit',
+            'attributes'    => array(               
+                'value' => __('Submit'),
+            ),
+            'type'  => 'submit',
+        ));
+    }
+}
---
+++ src/Model/Author.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Article module author class
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Copyright (c) http://www.eefocus.com
+ * @license         http://www.xoopsengine.org/license New BSD License
+ * @author          Zongshu Lin <zongshu@eefocus.com>
+ * @since           1.0
+ * @package         Module\Article
+ */
+
+namespace Module\Article\Model;
+
+use Pi;
+use Pi\Application\Model\Model;
+
+class Author extends Model
+{
+    public static function getAvailableFields()
+    {
+        return array('id', 'name', 'photo', 'description');
+    }
+
+    public function getSelectOptions()
+    {
+        $result = array('0' => '');
+
+        $select = $this->sql->select()
+            ->columns(array('id', 'name'))->order('name ASC');
+        $authors = $this->selectWith($select);
+
+        foreach ($authors as $author) {
+            $result[$author->id] = $author->name;
+        }
+
+        return $result;
+    }
+}
